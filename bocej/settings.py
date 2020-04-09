c588d39efe2ed08dbec051d6307934446dae5199
Oumar Barry <oumar.barry1202@gmail.com>
2020-04-09 15:27:54 +0000

    happY

--- bocej/settings.py
+++ bocej/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import dj-database-url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -16,7 +17,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['bocej.herokuapp.com']
+
 
 
 # Application definition
