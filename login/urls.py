5a108f172ff4fec660057fdf3553d6707a311392
Oumar Barry <oumar.barry1202@gmail.com>
2020-04-09 14:47:07 +0000

    rm login

--- login/urls.py
+++ login/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'login'
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
     path('logout', views.logout, name='logout'),
 ]
